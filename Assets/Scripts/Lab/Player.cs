69cacdb86a5dba0797e8081cd44653d99ddf5eff
athirujpao <athiruj.paop@bumail.net>
2024-09-24 16:43:33 +0700

    Create Scripts Character

---
+++ Assets/Scripts/Lab/Player.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    public void Shoot() 
+    { 
+
+    }
+    public void OnHitWithEnemy() { }
+}
